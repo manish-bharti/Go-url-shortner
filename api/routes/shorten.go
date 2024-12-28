f4f6d07e175850ad04b087f04b6ad8404ebec416
Manish bharti <parasparas0001@gmail.com>
2024-12-28 21:27:18 +0530

    add logic to shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -3,26 +3,37 @@ package routes
 import "time"
 
 type request struct {
-	URL					string					`json:"url"`
-	CustomShort			string					`json:"short"`
-	Expiry				time.Duration			`json:"expiry"`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
 }
 
 type response struct {
-	URL				string 						`json:"url"`
-	CustomShort		string						`json:"short"`
-	Expiry			time.Duration				`json:"expiry"`		
-	XRateRemaining	int							`json:"rate_limit"`
-	XrateLimitRest	time.Duration				`json:"rate_limit_reset"`
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
+	Expiry         time.Duration `json:"expiry"`
+	XRateRemaining int           `json:"rate_limit"`
+	XrateLimitRest time.Duration `json:"rate_limit_reset"`
 }
 
-
-func ShortenUrl(c *fiber.Ctx) error{
+func ShortenUrl(c *fiber.Ctx) error {
 
 	body := new(request)
 
-	if err := c.BodyParser(body); err != nil{
-
+	if err := c.BodyParser(body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
 	}
-}
 
+	//impment rate limiting
+
+	//check if input is actually a url
+
+	if !govalidator.IsUrl(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// enforce https . SSL
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+}
